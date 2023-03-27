b3d4876f9741cfc375fde4234c6a3234fff375e8
antar-cd <sagar.chandradas1994@gmail.com>
2023-03-28 01:14:18 +0600

    handlecart button added and working order summary

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
-import './Product.css'
+import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Product = (props) => {
-    const {img,name,seller,ratings,price}=props.product
+    //console.log(props)
+
+    const { img, name, seller, ratings, price } = props.product;
+
+    const handleAddToCart = props.handleAddToCart;
+
     return (
         <div className='product'>
             <img src={img} alt="" />
             <div className='product-info'>
-            <h6 className='product-name'>{name}</h6>
-            <p>Price: ${price}</p>
-            <p>ManuFacturer: {seller}</p>
-            <p>Rating: {ratings} stars</p>
+                <h6 className='product-name'>{name}</h6>
+                <p>Price: ${price}</p>
+                <p>ManuFacturer: {seller}</p>
+                <p>Rating: {ratings} stars</p>
             </div>
-            <button className='btn-cart'>Add to Cart</button>
+
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>
+                Add to Cart
+                <FontAwesomeIcon icon={faShoppingCart} />
+                </button>
         </div>
     );
 };

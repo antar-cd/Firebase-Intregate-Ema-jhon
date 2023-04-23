a4941ad433acc06ef5ec167bb30909ee6a869867
antar-cd <sagar.chandradas1994@gmail.com>
2023-04-23 20:34:31 +0600

    remining item & quantity added

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -43,7 +43,23 @@ const Shop = () => {
     const handleAddToCart=(product)=>{
         console.log('products',product)
         //cart.push(product)
-        const newCart = [...cart,product];
+
+        let newCart = [];
+
+        //const newCart = [...cart,product];
+        // if product does not exist in the cart , then set quantity = 1
+        // if exist update quantity by 1
+
+        const exists = cart.find(pd => pd.id === product.id);
+        if(!exists){
+            product.quantity = 1;
+            newCart= [...cart,product]
+        }
+        else{
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !==product.id);
+            newCart = [...remaining,exists]; 
+        }
         setCart(newCart);
         addToDb(product.id)
     }
